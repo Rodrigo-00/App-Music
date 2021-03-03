8d339cd40520451de84c9c6179289e8d21c17250
USUARIO <USUARIO@DESKTOP-L3T5T6K>
2021-03-03 19:33:15 +0100

    Implementado el registrar Playlist

--- AppMusic/src/umu/tds/modelo/Playlist.java
+++ AppMusic/src/umu/tds/modelo/Playlist.java
@@ -1,12 +1,34 @@
 package umu.tds.modelo;
 
+import java.util.LinkedList;
+import java.util.List;
+
+
+
 public class Playlist {
 	
+	private static final int IDINICIAL = 0;
+	
 	private String nombre;
+	private int id;
+	private List<Cancion> canciones;
 	
 	public Playlist(String nombre) {
+		id = IDINICIAL;
 		nombre = this.nombre;
+		canciones = new LinkedList<Cancion>();
 	}
+
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 
 	public String getNombre() {
 		return nombre;
@@ -14,5 +36,17 @@ public class Playlist {
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+	
+	public void addCancion(Cancion cancion) {
+		canciones.add(cancion);
+	}
+	
+	public void removeCancion(Cancion cancion) {
+		canciones.remove(cancion);
+	}
+
+	public List<Cancion> getCanciones() {
+		return canciones;
 	}
 }
--- AppMusic/src/umu/tds/persistencia/AdaptadorPlaylistTDS.java
+++ AppMusic/src/umu/tds/persistencia/AdaptadorPlaylistTDS.java
@@ -1,13 +1,27 @@
 package umu.tds.persistencia;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import beans.Entidad;
+import beans.Propiedad;
 import tds.driver.FactoriaServicioPersistencia;
 import tds.driver.ServicioPersistencia;
+import umu.tds.modelo.Cancion;
 import umu.tds.modelo.Playlist;
+import umu.tds.modelo.Usuario;
 
 public final class AdaptadorPlaylistTDS implements IAdaptadorPlaylistDAO{
 
 	private static ServicioPersistencia servPersistencia;
 	private static AdaptadorPlaylistTDS unicaInstancia;
+	
+	private static final String PLAYLIST = "Playlist";
+	private static final String NOMBRE = "nombre";
+	private static final String CANCIONES = "canciones";
+	
+	
 	
 	public static AdaptadorPlaylistTDS getUnicaInstancia() { // patron singleton
 		if (unicaInstancia == null) {
@@ -19,11 +33,45 @@ public final class AdaptadorPlaylistTDS implements IAdaptadorPlaylistDAO{
 	private AdaptadorPlaylistTDS() { 
 		servPersistencia = FactoriaServicioPersistencia.getInstance().getServicioPersistencia();
 	}
+	
+	
+	private Entidad playlistToEntidad(Playlist lista) {
+		Entidad ePlaylist = new Entidad();
+		ePlaylist.setNombre(PLAYLIST);
+		
+		ePlaylist.setPropiedades(new ArrayList<Propiedad>(Arrays.asList(
+								 new Propiedad(NOMBRE, lista.getNombre()),
+								 new Propiedad(CANCIONES, obtenerIdCanciones(lista.getCanciones())))));
+		
+		return ePlaylist;
+	}
+	
+	
 
 	@Override
 	public void registrarPlaylist(Playlist lista) {
-		// TODO Auto-generated method stub
 		
+		boolean existe = true;
+		Entidad ePlaylist;
+		try {
+			ePlaylist = servPersistencia.recuperarEntidad(lista.getId());
+		}catch (NullPointerException e) {
+			existe = false;
+		}
+		
+		if(existe) return;
+		ePlaylist = playlistToEntidad(lista);
+		ePlaylist = servPersistencia.registrarEntidad(ePlaylist);
+		lista.setId(ePlaylist.getId());
 	}
 	
+	
+	
+	private String obtenerIdCanciones(List<Cancion> listaCanciones) {
+		String aux = "";
+		for (Cancion c : listaCanciones) {
+			aux += c.getId() + " ";
+		}
+		return aux.trim();
+	}
 }
