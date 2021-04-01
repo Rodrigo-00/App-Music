1de84aff59aad8e9c60d130dc095c12f4e004a87
USUARIO <USUARIO@DESKTOP-L3T5T6K>
2021-04-01 21:06:57 +0200

    Todo bien hasta el momento, corregir ventanaExplorar se abre en pequeño

--- AppMusic/src/umu/tds/gui/VentanaExplorar.java
+++ AppMusic/src/umu/tds/gui/VentanaExplorar.java
@@ -85,7 +85,7 @@ public class VentanaExplorar {
 		GridBagLayout gbl_panel = new GridBagLayout();
 		gbl_panel.columnWidths = new int[]{10, 0, 10, 10, 0, 0, 0, 10, 68, 10, -3, 0};
 		gbl_panel.rowHeights = new int[]{10, 0, 20, 0};
-		gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 		
