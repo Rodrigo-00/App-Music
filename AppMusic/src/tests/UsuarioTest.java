225b37b5b26608725ca2c78f122882336f02b2d1
raul <raul@raul>
2021-06-29 19:52:22 +0200

    avance Junit

--- AppMusic/src/tests/UsuarioTest.java
+++ AppMusic/src/tests/UsuarioTest.java
@@ -5,7 +5,8 @@ import static org.junit.Assert.*;
 import java.util.Date;
 
 import org.junit.*;
-
+import java.util.LinkedList;
+import java.util.List;
 import umu.tds.modelo.Cancion;
 import umu.tds.modelo.Playlist;
 import umu.tds.modelo.Usuario;
@@ -13,6 +14,7 @@ import umu.tds.modelo.Usuario;
 public class UsuarioTest {
 	Usuario usuario;
 	Cancion cancion;
+	Cancion cancion1;
 	Playlist playlist;
 	Playlist playlist1;
 	
@@ -20,6 +22,7 @@ public class UsuarioTest {
 	public void usuario() {
 		usuario = new Usuario("Pedro", "Garcia", "p.garcia@um.es", "p.garcia", "pedro", new Date());
 		cancion = new Cancion("titulo", "interprete", "estilo", "rutaFichero");
+		cancion1 = new Cancion("titulo1", "interprete1", "estilo1", "rutaFichero1");
 		playlist = new Playlist("nombre");
 		playlist1 = new Playlist("nombre1");
 	}
@@ -46,16 +49,37 @@ public class UsuarioTest {
 
 	@Test
 	public void testNombresListas() {
-		fail("Not yet implemented");
+		usuario.addPlayList(playlist);
+		usuario.addPlayList(playlist1);
+		List<String> nombres = new LinkedList<String>();
+		nombres.add(playlist.getNombre());
+		nombres.add(playlist1.getNombre());
+		for (String nombre: usuario.nombresListas()) {
+			if(!nombre.contains(nombre))
+				assertTrue(false);
+		}
+		assertTrue(true);
 	}
 
 	@Test
-	public void testGetCancionesPlaylist() {
-		fail("Not yet implemented");
+	public void testGetCancionesPlaylistExiste() {
+		usuario.addPlayList(playlist);
+		playlist.addCancion(cancion);
+		playlist.addCancion(cancion1);
+		assertSame(playlist.getCanciones(), usuario.getCancionesPlaylist("nombre"));
+	}
+	
+	@Test
+	public void testGetCancionesPlaylistNoExiste() {
+		assertTrue(usuario.getCancionesPlaylist("nombre").isEmpty());
 	}
 
 	@Test
-	public void testOtorgarDescuento() {
+	public void testOtorgarDescuentoMayorIgual65() {
+	}
+	
+	@Test
+	public void testOtorgarDescuentoMenor65() {
 		fail("Not yet implemented");
 	}
 
